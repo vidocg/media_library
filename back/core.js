effe62147cb1d63debae69ccd5247df4b8ecd6a1
Vidocg <grapovv@gmail.com>
2021-04-18 11:02:39 +0300

    Init backend module

---
+++ back/core.js
@@ -0,0 +1,13 @@
+var express         = require('express');
+const jsonParser = express.json()
+var app = express();
+
+app.listen(8181, function(){
+    console.log('Express server listening on port 8181');
+});
+
+app.use(jsonParser); // JSON parsing
+app.use(express.static(__dirname + "/public")); // static file serer ( public/  folder)
+app.get('/api', function (req, res) {
+    res.send('API is running');
+});
